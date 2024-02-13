487832cb83f63c6827fd73672ef0ba1a2c8e6323
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-02-13 20:57:09 +0000

    commit

--- Server-Side/Exercicios/Ex01.py
+++ Server-Side/Exercicios/Ex01.py
@@ -1,3 +1,12 @@
+# Praticando Loop
 
-def print():
-    print("Hello world")
+Quantidade_Aluno = int(input("Digite a quantidade de aluno(a): "))
+Quantidade_inicial = 0 
+
+while Quantidade_Aluno > Quantidade_inicial:
+
+    Nome_Aluno = input('digite o nome do aluno(a): ')
+    Nota_Aluno = float(input('Digite a nota do aluno(a): '))
+    Quantidade_inicial += 1
+    
+print('Loop terminado')
