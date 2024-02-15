78ba21f6d54d8f6de29693e6bb3f52523b02fc03
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-02-15 23:05:26 +0000

    commit

--- Server-Side/Projetos/Jogo-Forca/main.py
+++ Server-Side/Projetos/Jogo-Forca/main.py
@@ -5,17 +5,43 @@ print('Jogo da Forca PATOCKSX!')
 começar = input('Digite "jogar" para começar: ')
 
 def forca():
-    if começar == "jogar":
-        print('ESCOLHA A CATEGORIA!')
-        print(' 1 | Comidas\n 2 | frutas\n 3 | Animais\n 4 | Carro\n 5 | País\n 6 | Aleatorio\n 7 | Tudo')
-        Comidas = []
-        Frutas = []
-        Animais = []
-        Carro = []
-        categoria = int(input('Digitar o numero da categoria:'))
-    else:
-        print('vocÊ não inicializou o jogo! :(')
+    loopforca = True
+    
+    while loopforca:
 
-print('a funçao acabou')
+        if começar == "jogar":
+            print('ESCOLHA A CATEGORIA!')
+            print(' 1 | Comidas\n 2 | frutas\n 3 | Animais\n 4 | Carro\n 5 | País\n 6 | Aleatorio\n 7 | Tudo\n 8 | Fechar')
+            Comidas = ['arroz', 'churrasco', 'jeijão']
+            Frutas = ['morango', 'uva', 'manga']
+            Animais = ['gato', 'cachorro', 'rato']
+            Carro = ['bmw', 'ferrari', 'ford']
+            categoria = int(input('Digitar o numero da categoria:'))
+            if categoria == 1:
+                print('1')
+
+            if categoria == 2:
+                print('2')
+
+            if categoria == 3:
+                print('3')
+
+            if categoria == 4:
+                print('4')
+
+            if categoria == 5:
+                print('5')
+
+            if categoria == 6:
+                print('6')
+
+            if categoria == 7:
+                print('7')
+
+            if categoria == 8:
+                loopforca = False
+
+
+print('o jogo acabou')
 
 forca()
