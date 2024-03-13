9664c4ad3c2f2be2774222a03f171f9403a622dd
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-03-13 20:31:40 +0000

    commit

---
+++ Client-Side/CSS/Exercicios/Ex01/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector('#botaop').addEventListener('click', cadastrarnoti)
+
+function cadastrarnoti() {
+    document.getElementsByTagName('section')[0].style.visibility = 'visible';
+    document.getElementsByTagName('h1')[0].style.visibility = 'hidden'
+
+}
