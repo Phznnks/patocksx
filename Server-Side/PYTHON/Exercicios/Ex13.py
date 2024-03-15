5fcc6e2d223d48d060b97ecd79b56fab58bc4794
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-03-15 12:18:08 +0000

    commit

--- Server-Side/PYTHON/Exercicios/Ex13.py
+++ Server-Side/PYTHON/Exercicios/Ex13.py
@@ -1,6 +1,30 @@
-import random
+#Syntax basica
+#for elemento_da_sequencia in sequencia:
+#    print(elemento_da_sequencia)
 
-teste = ['1', '2', '3', '4', '5']
+#For por uma sequencia 
+frutas = ['melancia', 'goiaba', 'manga', 'banada', 'abacaxi']
 
-for k in teste:
-    print(k)
+for fruta in frutas:
+    print(fruta)
+
+#For pegando o indice da sequencia, Usando enumerate
+frutas = ['melancia', 'goiaba', 'manga', 'banada', 'abacaxi']
+for indice, fruta in enumerate(frutas):
+    print(f'Indice: {indice} da Fruta: {fruta}')
+
+#For usando variais listas com o zip
+frutas = ['melancia', 'goiaba', 'manga', 'banada', 'abacaxi']
+pais = ['canada', 'italia', 'suica', 'grecia', 'portugal']
+for fruta, pais in zip(frutas,pais):
+    print(f'Fruta: {fruta} > Pais: {pais}')
+
+#For de numero com inicio, fim e passo opcinal, Usando range
+for n in range(0,10,1):
+    print(n)
+
+#For por dicionario 
+dici = {"colete": 2, "agua": 5, "hamburguer": 3, "celular": 1, "lanterna": 1}
+
+for item,valor in dici.items():
+    print(f'Nome Item: {item} Valor: {valor}')
