2abae9fa0fbd154cf32a9d6e0402c88d294d554f
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-03-17 03:36:09 +0000

    commit

--- Client-Side/CSS/Exercicios/Ex01/scripts.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#botaop').addEventListener('click', cadastrarnoti)
-
-function cadastrarnoti() {
-    document.getElementsByTagName('section')[0].style.visibility = 'visible';
-    document.getElementsByTagName('h1')[0].style.visibility = 'hidden'
-
-}
--- Client-Side/JAVASCRIPT/Exercicios/Ex11/scripts.js
+++ Client-Side/JAVASCRIPT/Exercicios/Ex11/scripts.js
@@ -1,1 +1,12 @@
-console.log('testando')
+document.querySelector('#paraghoras')
+document.querySelector('#imghoras')
+
+var hora = 13
+
+if (hora >= 11) {
+    alert('manha')
+} else if (hora <= 12) {
+    alert('tarde')
+} else if (hora < 18) {
+    alert('noite')
+}
