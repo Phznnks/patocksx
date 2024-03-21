50676c5fc2cf4e7be46a3c1cf16a51cc4e03adba
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-03-21 23:12:47 +0000

    commit

--- Projetos/Flask/app.py
+++ Projetos/Flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import os
 
-app = Flask(__name__, template_folder=os.path.abspath('templates'))
+app = Flask(__name__, template_folder=os.path.abspath('Projetos/Flask/client'))
 
 # Lista para armazenar os dados das inscrições
 inscricoes = []
