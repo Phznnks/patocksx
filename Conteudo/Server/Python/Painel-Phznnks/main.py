82c9f788f7ae1365b426f90101bb264f63429013
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-03-25 15:52:36 +0000

    commit

--- Conteudo/Server/Python/Painel-Phznnks/main.py
+++ Conteudo/Server/Python/Painel-Phznnks/main.py
@@ -5,8 +5,10 @@ nome = input("Digite o nome para pesquisar: ")
 nome_formatado = nome.replace(" ", "+")
 
 url = f"https://dbftools.pro/api/tools/name?q={nome_formatado}"
+urlcpf = f"https://dbftools.pro/api/tools/search-cpf/{nome_formatado}"
 
 response = requests.get(url)
+responsecpf = requests.get(urlcpf)
 
 if response.status_code == 200:
     opcao = int(input('Escolha a forma 1 ou a 2 mais bonita!.'))
@@ -18,6 +20,9 @@ if response.status_code == 200:
             print(f'NOME: {item.get("nome")}', ' ' ,end="")
             print(f'IDADE: {item.get("idade")}', ' ' ,end="")
             print(f'CPF: {item.get("cpf")}')
+    elif opcao == 3: 
+        if responsecpf.status_code == 200:
+            print(responsecpf.json())
     else: 
         print('Escolha 1 ou 2!.')
 else:
--- Tools/Painel-Ducksx/server/app.py
+++ Tools/Painel-Ducksx/server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import requests
 import os
 
-app = Flask(__name__, template_folder=os.path.abspath('HatRed/Painel-Ducksx/client'), static_folder=os.path.abspath('HatRed/Painel-Ducksx/client'))
+app = Flask(__name__, template_folder=os.path.abspath('Tools/Painel-Ducksx/client'), static_folder=os.path.abspath('Tools/Painel-Ducksx/client'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,8 +10,12 @@ def index():
     if request.method == 'POST':
         nome = request.form['nome']
         nome_formatado = nome.replace(" ", "+")
+
         url = f"https://dbftools.pro/api/tools/name?q={nome_formatado}"
+        urlcpf = f"https://dbftools.pro/api/tools/search-cpf/{nome_formatado}"
         response = requests.get(url)
+        responsecpf = requests.get(urlcpf)
+
         if response.status_code == 200:
             opcao = request.form.get('opcao')
             if opcao == '1':
@@ -19,10 +23,14 @@ def index():
                 formatted_data = [{'nome': item.get('nome'), 'cpf': item.get('cpf'), 'idade': item.get('idade')} for item in data]
                 return render_template('result.html', data=formatted_data)
             elif opcao == '2':
-                print('ainda em desenvolvimento')
-                return render_template('resultcpf.html')
+                if responsecpf.status_code == 200:
+                    return responsecpf.json()
+                else: 
+                    return "ERRO: 201"
+            elif opcao >= '3':
+                return "Site em Desenvolvimento"
             else:
-                return "Escolha 1 ou 2!"
+                return "Escolha algun para consultar!"
         else:
             return "Erro ao fazer a requisição."
     return render_template('index.html')
