1bc1a35c9a7065bb73b1857a28bb74cc69ee5077
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-04-01 14:01:47 +0000

    commit

--- Conteudo/Server/Python/Painel-Phznnks/main.py
+++ Conteudo/Server/Python/Painel-Phznnks/main.py
@@ -1,15 +1,10 @@
 import requests
 
-nome = input("Digite o nome para pesquisar: ")
-
-nome_formatado = nome.replace(" ", "+")
-
-url = f"https://dbftools.pro/api/tools/name?q={nome_formatado}"
-urlcpf = f"https://dbftools.pro/api/tools/search-cpf/{nome_formatado}"
-
-response = requests.get(url)
+urlcpf = f"https://dbftools.pro/api/tools/search-cpf/07068093868"
 responsecpf = requests.get(urlcpf)
+print(responsecpf.json())
 
+'''
 if response.status_code == 200:
     opcao = int(input('Escolha a forma 1 ou a 2 mais bonita!.'))
     if opcao == 1:
@@ -27,3 +22,4 @@ if response.status_code == 200:
         print('Escolha 1 ou 2!.')
 else:
     print("Erro ao fazer a requisição.")
+'''
