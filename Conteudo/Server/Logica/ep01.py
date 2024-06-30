b05eab9351dd6291ce2e1b1ad667a8fc656039cf
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-06-30 21:46:56 +0000

    commit

--- Conteudo/Server/Logica/ep01.py
+++ Conteudo/Server/Logica/ep01.py
@@ -1,3 +1,19 @@
-[
-    
-]
+'''
+A ideia da logica e criar um programa que orgraniza uma lista
+em x ordem, Exemplo: Se fosse uma lista de nomes de pessoas, ela vai se
+organizar de A a Z, ou gerar numeros aleatorios de x em x e enverte para a ordem deles.
+'''
+
+''' SIGNIFICADO DAS VARIAVEIS
+TOTALN -- TOTAL DE NOMES QUE A PESSOA QUE COLOCA.}
+VALORAT -- VALOR ATUAL 
+'''
+
+valorat = 0
+totaln = int(input('Informe a quantidade de pessoa que estará na lista: '))
+nomes = []
+
+while totaln > valorat: 
+    nomes = nomes + [str(input('Informe o nome da pessoa: '))]
+    valorat = valorat + 1
+print(nomes)
---
+++ Conteudo/Server/Node/Atividades/ep01.js
@@ -0,0 +1,6 @@
+console.log(`tantes`)
+n1 = 3.1415
+nd = n1 / 2
+console.log(nd)
+nd2 = n1 % 2
+console.log(nd2)
