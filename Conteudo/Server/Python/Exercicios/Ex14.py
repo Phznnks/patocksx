14d80cd2c5f6c8b7ca3f735539a5ae621b338dc1
Phznnks <149673913+Phznnks@users.noreply.github.com>
2024-07-09 21:45:08 +0000

    commit

--- Conteudo/Server/Python/Exercicios/Ex14.py
+++
@@ -1,5 +0,0 @@
-for n in range(10,-1,-1):
-    print(n)
-
-print('Bummmmm')
-print('Fogos foram ativado')
--- Conteudo/Server/Python/Exercicios/Ex15.py
+++
@@ -1,4 +0,0 @@
-for i in range(0, 52, 2):
-    print(f'{i}', " ",end='')
-
-print('Acabou')
